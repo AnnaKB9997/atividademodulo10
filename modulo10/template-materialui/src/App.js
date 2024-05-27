2fc14886b8a0334ee70333e8da4d847d4c5ef3b5
anna.brisola <anna.k.brisola@gmail.com>
2024-05-27 19:35:20 -0300

    minhas modificacoes

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,11 +1,22 @@
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000",
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ListarTarefa />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <ListarTarefa />
+      </ThemeProvider>
     </div>
   );
 }
